7d67c7cf3d8b2ef3737f55a060af55962981f9bf
moa-mel <52823454+moa-mel@users.noreply.github.com>
2023-07-23 21:44:27 +0100

    update

--- src/pages/Settings/index.js
+++ src/pages/Settings/index.js
@@ -1,18 +1,77 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./styles.css"
 import Sidebar from "../../components/Sidebar"
 import Navbar from "../../components/Navbar"
+import phone from "../../images/fluent_phone-add-24-regular.png"
+import mdi from "../../images/mdi_email-sync-outline.png"
+import 'react-phone-number-input/style.css'
+import PhoneInput from 'react-phone-number-input'
 
 const Settings = () => {
-    return (
-        <div className='setting'>
-        <Navbar />
-        <div className='setting-main'>
-          <Sidebar />
-          <div className='setting-content'>
-          
+  const [value, setValue] = useState()
+  return (
+    <div className='setting'>
+      <Navbar />
+      <div className='setting-main'>
+        <Sidebar />
+        <div className='setting-content'>
+          <div className='set-top'>
+            <div className='set-tp'>
+              <img className='set-tp-img' src={phone} alt='' />
+              <div className='set-t'>
+                <p className='set-t-p'>Change or Add Phone No</p>
+                <p className='set-t-p1'>Delete Phone No.</p>
+              </div>
+            </div>
+        {/*<div className="set-input"> */}
+              <PhoneInput
+                placeholder="Enter phone number"
+                country="NG"
+                value={value}
+                name="phoneNumber"
+                type="text"
+                onChange={setValue}
+                className="set-top-input"
+                containerClass="my-container-class"
+                inputClass="my-input-class"
+                containerStyle={{
+                  border: "10px solid #E1E1E1",
+                  background: "E1E1E1"
+                }}
+                inputStyle={{
+                  background: "E1E1E1"
+                }}
+              />
+             {/* </div> */}
+          </div>
+          <div className='set-down'>
+            <div className='set-dn'>
+              <img className='set-dn-img' src={mdi} alt="" />
+              <div className='set-d'>
+                <p className='set-d-p'>E-mail Address Settings</p>
+                <p className='set-d-p1'>Delete E-mail address</p>
+              </div>
+            </div>
+            <p className='set-down-p'>Click here to verify Email Address</p>
+            <label className="set-down-label">
+              Change Personal E-mail Address</label> <br />
+            <input
+              className="set-down-input"
+              type="email"
+              placeholder="Enter Personal E-mail Address"
+            /><br />
+            <label className="set-down-label">
+              Change Personal E-mail Address</label> <br />
+            <input
+              className="set-down-input"
+              type="email"
+              placeholder="Enter Organization E-mail Address"
+            /><br />
+            <button className="set-down-butt">Submit</button>
           </div>
         </div>
-        </div>
-    )}
+      </div>
+    </div>
+  )
+}
 export default Settings
