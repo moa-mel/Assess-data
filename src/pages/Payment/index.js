121b2fdeead0e54fade0a6e26bd11093416655a9
moa-mel <52823454+moa-mel@users.noreply.github.com>
2023-07-26 16:15:10 +0100

    fixed transaction history

--- src/pages/Payment/index.js
+++ src/pages/Payment/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
 import './styles.css'
 import { Select } from '@chakra-ui/react'
 import Logo from "../../images/Oau_logo-removebg-preview 2.png"
@@ -18,36 +18,49 @@ import G20 from "../../images/Group 20.png"
 import sort from "../../images/sort 2.png"
 import sch from "../../images/Search.png"
 import dots from "../../images/ph_dots-three-vertical-thin.png"
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 import { Progress } from '@chakra-ui/react'
 import axios from 'axios';
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from '@chakra-ui/react'
 
-const Payment = () =>{
+
+const Payment = ({paymentSuccessful}) => {
   const [activeTab, setActiveTab] = useState(0);
   const [paymentInitialized, setPaymentInitialized] = useState(false);
-  const [url, setUrl]  = useState('')
-  console.log(url)
+  const [url, setUrl] = useState('')
+  const [transactions, setTransactions] = useState([]);
+  console.log('transaction')
   const handleTabClick = (index) => {
     setActiveTab(index);
   };
 
   const initializePayment = async (e) => {
 
-    const token =  localStorage.getItem("accessToken")
+    const token = localStorage.getItem("accessToken")
     console.log("okay")
     e.preventDefault()
     try {
-      const response = await axios.post('https://final-year-project-ya34.onrender.com/api/transaction/make-payment', 
-      {
-        
-      }, 
-      {
-        headers: {
-          'Content-Type': 'application/json',
-          "x-auth-token": `${token}`,
-          // Add any required headers here, such as authentication tokens
+      const response = await axios.post('https://final-year-project-ya34.onrender.com/api/transaction/make-payment',
+        {
+
         },
-      });
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            "x-auth-token": `${token}`,
+            // Add any required headers here, such as authentication tokens
+          },
+        });
       if (response.data.success) {
         // Payment initialized successfully
         setUrl(response.data.initializationUrl);
@@ -56,10 +69,40 @@ const Payment = () =>{
         // Handle error if needed
       }
     } catch (error) {
+      console.log("hewww", error)
       // Handle any network or other errors
     }
   };
- 
+
+    // Fetch transaction history from the backend/API
+    const fetchTransactionHistory = async () => {
+      const token = localStorage.getItem("accessToken")
+      try {
+        const response = await axios.post('https://final-year-project-ya34.onrender.com/api/transaction/transaction-history',
+        {
+
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            "x-auth-token": `${token}`,
+            // Add any required headers here, such as authentication tokens
+          },
+        }
+        );
+        const transactionsData = response.data.transactionHistory;
+        console.log('work', transactionsData)
+         // Assuming the response data is an array of transactions
+        setTransactions(transactionsData);
+      } catch (error) {
+        console.error('Error fetching transaction history:', error);
+      }
+    } 
+
+ useEffect(() =>{
+  fetchTransactionHistory()
+} ,[fetchTransactionHistory, paymentInitialized]) 
+
   return(
     <div className='payment'>
       {/*header*/}
@@ -104,8 +147,8 @@ const Payment = () =>{
               <p className='pay-side-right-p1'>Settings</p> <br />
               <p className='pay-side-right-p2'></p><br />
               <Link to='/feedback'>
-              <p className='pay-side-right-p'>Feedback</p> <br />
-             </Link>
+                <p className='pay-side-right-p'>Feedback</p> <br />
+              </Link>
               <p className='pay-side-right-p'>Notification</p> <br /><br />
               <p className='pay-side-right-p'>LogOut</p>
             </div>
@@ -117,23 +160,23 @@ const Payment = () =>{
             <div className="pay-top-left">
               <div className="pay-top">
                 <div className="pay-toppp">
-                <Link to='/subscription'>
-                  <h2 className="pay-left-h2">Subscription</h2> 
+                  <Link to='/subscription'>
+                    <h2 className="pay-left-h2">Subscription</h2>
                   </Link>
                   <div className="py-toppp">
-                  {!paymentInitialized ? (
-                    <p className="py-toppp-p" onClick={initializePayment}>plan</p>
+                    {!paymentInitialized ? (
+                      <p className="py-toppp-p" onClick={initializePayment}>plan</p>
                     ) : (
                       <p>Payment Initialized!</p>
                     )}
                     <Link to={url}>
-                    <button className="py-toppp-but" >Pay per Report</button>
+                      <button className="py-toppp-but" >Pay per Report</button>
                     </Link>
                   </div>
                   <div className="py-hero">
                     <div className="py-wrap">
-                    <p className="py-wrap-p">30 of 500 users</p>
-                    <Progress colorScheme='purple' size='sm' value={20} />
+                      <p className="py-wrap-p">30 of 500 users</p>
+                      <Progress colorScheme='purple' size='sm' value={20} />
                     </div>
                     <button className="py-h-but">Upgrade Plan</button>
                   </div>
@@ -145,58 +188,75 @@ const Payment = () =>{
               </div>
             </div>
             <div className="pay-top-right">
-              <p className="pay-right-p1">Next Payment </p> <br/>
+              <p className="pay-right-p1">Next Payment </p> <br />
               <p className="pay-right-p2">on February 27, 2021</p> <br />
               <button className="pay-right-button">Manage payment</button> <br />
             </div>
           </div>
 
           <div className='pay-content-down'>
-             <div className='pay-down-up'>
-             <p className='pay-down-p1'>Payment</p>
-             <p 
-             className={activeTab === 0 ? 'active' : ''}
-          onClick={() => handleTabClick(0)}
-             >All payment</p>
-             <p 
-             className={activeTab === 1 ? 'active' : ''}
-             onClick={() => handleTabClick(1)}
-             >Failed</p>
-             <p 
-             className={activeTab === 2 ? 'active' : ''}
-          onClick={() => handleTabClick(2)}
-             >Complete</p>
-             <div className='pay-down-side'>
-             <img className='pay-down-img' src={sort} alt='' />
-             <img className='pay-down-ig' src={sch} alt='' />
-             </div>
-             </div>
-             <br/>
-             
-             {activeTab === 0 &&
-              <>
-             <div className="pay-other">  
-             <div className='pay-other-one'>
-                <img className='pay-other-1' src={G20} alt=''/>
-                <p className='pay-other-p'>Invoice_2021/10.pdf</p>
-                <p className='pay-other-p1'>Date of invoice</p>
-                <p className='pay-other-p2'>Oct 02, 2021</p>
-                <img className='pay-other-img' src={dots} alt=''/>
+            <div className='pay-down-up'>
+              <p className='pay-down-p1'>Payment</p>
+              <p
+                className={activeTab === 0 ? 'active' : ''}
+                onClick={() => handleTabClick(0)}
+              >All payment</p>
+              <p
+                className={activeTab === 1 ? 'active' : ''}
+                onClick={() => handleTabClick(1)}
+              >Failed</p>
+              <p
+                className={activeTab === 2 ? 'active' : ''}
+                onClick={() => handleTabClick(2)}
+              >Complete</p>
+              <div className='pay-down-side'>
+                <img className='pay-down-img' src={sort} alt='' />
+                <img className='pay-down-ig' src={sch} alt='' />
               </div>
-             </div>
-             
-            </>
-            }
-            
-             <div className='' id="Failed">
-             {activeTab === 1 &&
-             <p>Failed, sorry!</p> }
-             </div>
+            </div>
+            <br />
 
-             <div className='' id="Complete">
-             {activeTab === 2 &&
-             <p>complete, yeah.</p> }
-             </div>
+            {activeTab === 0 &&
+              <>
+                <div className="pay-other">
+                  <div className='pay-other-one'>
+                    <img className='pay-other-1' src={G20} alt='' />
+                    <p className='pay-other-p'>Invoice_2021/10.pdf</p>
+                    <p className='pay-other-p1'>Date of invoice</p>
+                    <p className='pay-other-p2'>Oct 02, 2021</p>
+                    <img className='pay-other-img' src={dots} alt='' />
+                  </div>
+                </div>
+                {transactions?.map(( transaction) =>(
+                  <TableContainer>
+  <Table size='sm'>
+    <Thead>
+      <Tr>
+        <Th>TransactionReference</Th>
+        <Th>Amount</Th>
+      </Tr>
+    </Thead>
+    <Tbody>
+      <Tr>
+        <Td>{transaction.transactionReference}</Td>
+        <Td>{transaction.amount}</Td>
+      </Tr>
+    </Tbody>
+  </Table>
+</TableContainer>
+               ))}
+              </>
+            }
+
+            <div className='' id="Failed">
+              {activeTab === 1 &&
+                <p>Failed, sorry!</p>}
+            </div>
+
+            <div className='' id="Complete">
+              {activeTab === 2 &&
+                <p>complete, yeah.</p>}
+            </div>
 
           </div>
         </div>
